706a22575a1977b2f5c1cc7bfb534813a0b54e45
sirichakra <147473243+sirichakra@users.noreply.github.com>
2023-10-10 11:20:11 +0530

    Add files via upload

---
+++ demo.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a,b, sum;
+    printf("enter the value of a");
+    prntf("enter the value ofb");
+    scanf("%d",a,b);
+    sum=a+b;
+    printf("%d",sum);
+}
